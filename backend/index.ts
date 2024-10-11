9a67c1dd3feb47cdcad6df94aaa48f7196898473
Jesus Gollonet <jesusgollonet@gmail.com>
2024-10-11 17:59:59 +0200

    Switch to fastify + tsx

--- backend/index.ts
+++ backend/index.ts
@@ -1,8 +1,44 @@
-import express from "express";
+import fastify, { FastifyInstance } from "fastify";
 
-const app = express();
-const port = process.env.PORT || 3000;
+const server: FastifyInstance = fastify();
 
-app.listen(3000, () => {
-  console.log("server is running");
+interface User {
+  id: number;
+  name: string;
+}
+
+const users: User[] = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Bob" },
+];
+
+const addUser = (user: User) => {
+  user.id = users.length + 1;
+  users.push(user);
+};
+
+server.get("/users", async (request, reply) => {
+  return users;
+});
+
+server.post<{ Body: User }>("/users", async (request, reply) => {
+  // yolo validation
+  if (!request.body.name) {
+    reply.code(400);
+    return { error: "Name is required" };
+  } else if (users.some((user) => user.name === request.body.name)) {
+    reply.code(409);
+    return { error: "Name is already taken" };
+  }
+  addUser(request.body);
+  return { user: request.body };
+});
+
+server.listen({ port: 3000 }, (err, address) => {
+  console.log("h");
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
 });
